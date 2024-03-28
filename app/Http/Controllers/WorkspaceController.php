2db7d128dbae86c43bdf51cd6f507ba5ec6fa1da
NEVAN FAUZI <momominky79@gmail.com>
2024-03-28 13:16:03 +0700

    "error Array to string conversion "

--- app/Http/Controllers/WorkspaceController.php
+++ app/Http/Controllers/WorkspaceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Teams;
 use App\Models\User;
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class WorkspaceController extends Controller
@@ -34,24 +35,46 @@ class WorkspaceController extends Controller
         if ($error) {
           echo "cURL Error #:" . $error;
         } else {
-//$save = $this->store($response);
-          echo $response->teams;
+          $response=json_decode($response,true);
+          $save = $this->store($response);
+          echo $save;
         }
         
     }
 
     public function store($data)
     {
+
+      // dd($data);
       
-      dd($data);
-        foreach ($data as $key => $value) {
+        foreach ($data['teams'] as $key => $value) {
+          $tabelMembers= Member::with('User')->get();
+          //dd($value['members']);
+          //dd($data);
+          foreach ($value['members'] as $key => $value_m) {
+          //  dd($value_m['user']);
+            $user = User::create([
+              'username' => $value_m['user']['username'],
+              'email' => $value_m['user']['email'],
+              'color' => $value_m['user']['color'],
+              'profilePicture' => $value_m['user']['profilePicture'],
+              'initials' => $value_m['user']['initials'],
+              'role' => $value_m['user']['role'],
+              'custom_role' => $value_m['user']['custom_role'],
+              'last_active' => $value_m['user']['last_active'],
+              'date_joined' => $value_m['user']['date_joined'],
+              'date_invited' => $value_m['user']['date_invited'],
+            ]);
+          }
           
-          
+          //dd($value['teams']);
           // save workspace
           $team = Teams::create([
-            'name' => $value->name,
-            'color' => $value->color,
-            'avatar' => $value->avatar
+            'name' => $value['name'],
+            //['teams']['name'],
+            'color' => $value['color'],
+            'avatar' => $value['avatar'],
+            'members' => $value['members'],
           ]);
         }
         
